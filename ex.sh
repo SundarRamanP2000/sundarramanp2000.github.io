83defd5de0d59418324d574cef4ba0805648f07d
Sundar Raman P <sundarramanp2000@gmail.com>
2020-03-28 23:23:56 +0530

    'fin'

--- ex.sh
+++ ex.sh
@@ -1,6 +1,8 @@
-git add .
-git commit -m 'fin'
 #!/usr/bin/expect 
+spawn git add .
+expect eof
+spawn git commit -m 'fin'
+expect eof
 set name sundarramanp2000
 set roll Sund@Priya1!
 spawn git push
