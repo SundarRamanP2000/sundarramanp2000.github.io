e2b7785de6348407f75b938c0828ca05c4c0166f
Sundar Raman P <sundarramanp2000@gmail.com>
2020-03-28 23:23:08 +0530

    fin

--- ex.sh
+++ ex.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-
 git add .
-git commit -m 'fi'
-echo -e "sundarramanp2000\n" "Sund@Priya1!\n" | git push
+git commit -m 'fin'
+#!/usr/bin/expect 
+set name sundarramanp2000
+set roll Sund@Priya1!
+spawn git push
+expect "Username for 'https://github.com': "
+send -- "$name\r"
+expect "Password for 'https://sundarramanp2000@github.com': "
+send -- "$roll\r"
+expect eof
+ 	
+
